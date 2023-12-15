cbe4144d568d3d1d1eca8ae62989ecc800adc17b
Ru Kai <will.ki.ru@hotmail.com>
2023-12-15 17:01:05 +0800

    refactor code

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -2,18 +2,19 @@ use std::{fs::File, io::{BufReader, BufRead}};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Rock {
-    Round,
-    Cube,
+    Round(usize),
+    Cube(usize),
 }
 
 struct Game {
-    cols: Vec<Vec<Option<Rock>>>,
+    cols: Vec<Vec<Rock>>,
+    size: (usize, usize),  // number of rows, number of cols
 }
 
 impl Game {
     
     fn new() -> Self {
-        Self { cols: Vec::new() }
+        Self { cols: Vec::new(), size: (0, 0) }
     }
 
     fn add_row(&mut self, line: &str) {
@@ -22,31 +23,33 @@ impl Game {
             for _ in 0..line.len() {
                 self.cols.push(Vec::new());
             }
+            self.size.1 = line.len();
         }
+        let row_idx = self.size.0;
         for i in 0..line.len() {
-            self.cols[i].push(match line[i] {
-                b'O' => Some(Rock::Round),
-                b'#' => Some(Rock::Cube),
-                b'.' => None,
-                _ => panic!("Invalid character"),
-            });
+            match line[i] {
+                b'O' => self.cols[i].push(Rock::Round(row_idx)),
+                b'#' => self.cols[i].push(Rock::Cube(row_idx)),
+                b'.' => {},
+                _ => unreachable!(),
+            }
         }
+        self.size.0 += 1;
     }
 
-    fn calculate_part1(&self) -> usize {
+    fn calculate_load(&self) -> usize {
         let mut load = 0;
         for col in self.cols.iter() {
-            let mut score = col.len();
-            for i in 0..col.len() {
-                match col[i] {
-                    Some(Rock::Round) => {
+            let mut score = self.size.0;
+            for r in col.iter() {
+                match *r {
+                    Rock::Cube(row_idx) => {
+                        score = self.size.0 - row_idx - 1;
+                    },
+                    Rock::Round(_) => {
                         load += score;
                         score -= 1;
-                    },
-                    Some(Rock::Cube) => {
-                        score = col.len() - i - 1;
-                    },
-                    None => {}
+                    }
                 }
             }
         }
@@ -65,10 +68,9 @@ fn main() {
             break;
         }
         game.add_row(line.trim());
-
         line.clear();
     }
-    let part1 = game.calculate_part1();
+    let part1 = game.calculate_load();
     println!("Part1: {}", part1);
 
 }
