1b1743ecec3244480043368989b14a041461324d
Ru Kai <will.ki.ru@hotmail.com>
2023-12-23 10:07:23 +0800

    Finish Part2

--- 2023/day21/src/main.rs
+++ 2023/day21/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{VecDeque, HashSet},
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -51,19 +51,6 @@ impl MapBuilder {
     }
 }
 
-fn bfs(map: &Map, steps: usize) -> usize {
-    let mut points = HashSet::new();
-    points.insert(map.start.clone());
-    for _ in 0..steps {
-        let mut next_points = HashSet::new();
-        for point in points.iter() {
-            next_points.extend(map.get_neighbours(point));
-        }
-        points = next_points;
-    }
-    points.len()
-}
-
 impl Map {
     fn get_neighbours(&self, point: &(usize, usize)) -> Vec<(usize, usize)> {
         let mut neighbours = Vec::new();
@@ -93,7 +80,89 @@ impl Map {
         }
         neighbours
     }
+
+    fn get_neighbours_i64(&self, point: &(i64, i64)) -> Vec<(i64, i64)> {
+        let mut neighbours = Vec::with_capacity(4);
+        let next = (point.0 - 1, point.1);
+        if self.get_grids_i64(&next) {
+            neighbours.push(next);
+        }
+        let next = (point.0 + 1, point.1);
+        if self.get_grids_i64(&next) {
+            neighbours.push(next);
+        }
+        let next = (point.0, point.1 - 1);
+        if self.get_grids_i64(&next) {
+            neighbours.push(next);
+        }
+        let next = (point.0, point.1 + 1);
+        if self.get_grids_i64(&next) {
+            neighbours.push(next);
+        }
+        neighbours
+    }
+
+    fn get_grids_i64(&self, point: &(i64, i64)) -> bool {
+        let mut point = (point.0 % (self.size.0 as i64), point.1 % (self.size.1 as i64));
+        if point.0 < 0 {
+            point.0 += self.size.0 as i64;
+        }
+        if point.1 < 0 {
+            point.1 += self.size.1 as i64;
+        }
+        assert!(point.0 >= 0 && point.1 >= 0);
+        self.grids[point.0 as usize][point.1 as usize]
+    }
 }
+
+fn bfs(map: &Map, steps: usize) -> usize {
+    let mut points = HashSet::new();
+    points.insert(map.start.clone());
+    for _ in 0..steps {
+        let mut next_points = HashSet::new();
+        for point in points.iter() {
+            next_points.extend(map.get_neighbours(point));
+        }
+        points = next_points;
+    }
+    points.len()
+}
+
+fn valid_grids(map: &Map) -> HashSet<(usize, usize)> {
+    let mut points = HashSet::new();
+    let mut valid_points = HashSet::new();
+    valid_points.insert(map.start.clone());
+    points.insert(map.start.clone());
+    while points.len() != 0 {
+        let mut next_points = HashSet::new();
+        for point in points.iter() {
+            for next_point in map.get_neighbours(point) {
+                if valid_points.contains(&next_point) {
+                    continue;
+                }
+                valid_points.insert(next_point.clone());
+                next_points.insert(next_point);
+            }
+        }
+        points = next_points;
+    }
+    valid_points
+}
+
+
+fn infinit_bfs(map: &Map, steps: usize) -> usize {
+    let mut points = HashSet::new();
+    points.insert((map.start.0 as i64, map.start.1 as i64));
+    for _ in 0..steps {
+        let mut next_points = HashSet::new();
+        for point in points.iter() {
+            next_points.extend(map.get_neighbours_i64(point));
+        }
+        points = next_points;
+    }
+    points.len()
+}
+
 
 fn main() {
     let f = File::open("./input").unwrap();
@@ -111,4 +180,26 @@ fn main() {
 
     let part1 = bfs(&map, 64);
     println!("Part1 {}", part1);
+    println!("Start {:?}", map.start); // (65, 65)
+    println!("{:?}", map.size); // (131, 131)
+    let steps = 26501365;
+    let s_repeats = steps / map.size.0; // 202300
+    let s_remains = steps % map.size.0; // 65
+    
+    // 34920, 96829, 189644, 313365 ...
+    //    61909, 92815, 123721 ...
+    //       30906, 30906 ...
+    let mut values = Vec::new();    
+    for i in 1..4 {
+        let s = s_remains + map.size.0 * i;
+        let count = infinit_bfs(&map, s);
+        println!("{}: {}", i, count);
+        values.push(count);
+    }
+    let delta1 = values[1] - values[0];
+    let delta2 = values[2] - values[1];
+    let delta_inc = delta2 - delta1;
+    let part2 = values[0] + (delta1 + delta1 + delta_inc * (s_repeats - 2)) * (s_repeats - 1) / 2;
+    println!("Part2 {}", part2);
+
 }
